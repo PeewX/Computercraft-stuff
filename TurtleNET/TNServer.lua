60fdd93ee43edebbec8e7d4ff12ac8d3e6c93978
namo2 <Namo_2@gmx.de>
2021-03-05 14:47:06 +0100

    added InfoMsg and GPS

--- TurtleNET/TNServer.lua
+++ TurtleNET/TNServer.lua
@@ -20,11 +20,31 @@ function TN.updateMonitor()
     mon.setCursorPos(1, 2)
     mon.write("Registered Turtles:")
     if #TN.registered > 0 then
+        local posX = 1
         for k, v in pairs(TN.registered) do
-            mon.setCursorPos(1, 2+k)
+            mon.setCursorPos(1, 2+posX)
             mon.write(v.label)
-            mon.write(" ")
-            mon.write(math.ceil((os.epoch("utc") - v.lastContact)/1000))
+
+            if v.infoMsg then
+                posX = posX + 1
+                mon.setCursorPos(1, 2+posX)
+                mon.write("\t")
+                mon.write("Msg: \"" .. v.infoMsg .. "\"")
+            end
+            if v.gps then
+                posX = posX + 1
+                mon.setCursorPos(1, 2+posX)
+                mon.write(" ")
+                mon.write("Location: " .. v.gps[1] .. ", " .. v.gps[2] .. ", " .. v.gps[3])
+            end
+            if v.lastContact then
+                posX = posX + 1
+                mon.setCursorPos(1, 2+posX)
+                mon.write(" ")
+                mon.write("Last contact: " .. math.ceil((os.epoch("utc") - v.lastContact)/1000) .. "s ago")
+            end
+
+            posX = posX + 1
         end
     else
         mon.setCursorPos(1, 3)
@@ -35,7 +55,7 @@ end
 function TN.receive()
     local sId, message, dis = rednet.receive("TN", 1)
 
-    if message and message.cmd, message.data then
+    if message and message.cmd and message.data then
         if message.cmd == "register" then
             TN.register(sId, message.data)
         elseif message.cmd == "status" then
@@ -50,6 +70,8 @@ function TN.register(TId, data)
     local registerTable = {
         Id = TId,
         label = data.label,
+        infoMsg = nil,
+        gps = nil,
         lastContact = os.epoch("utc")
     }
 
@@ -58,6 +80,20 @@ function TN.register(TId, data)
 end
 
 function TN.recvInfo(TId, data)
-    print("Turtle " .. TId .. ":\n")
-    print("X: " .. data.gps[1] .. " | Y: " .. data.gps[2] .. " | Z: " .. data.gps[3])
+    for k, v in pairs(TN.registered) do
+        if v.Id == TId then
+            if data.infoMsg then
+                TN.registered[k].infoMsg = data.infoMsg
+            end
+
+            if data.gps then
+                TN.registered[k].gps = data.gps
+            end
+            v.lastContact = os.epoch("utc")
+        end
+    end
 end
+
+------------------
+TN.updateMonitor()
+while true do TN.receive() end
